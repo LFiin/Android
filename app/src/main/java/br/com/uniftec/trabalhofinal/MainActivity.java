68a1f3880a0bb5bf95fe05c48691a39bdebccbbb
DESKTOP-B55SLIG\lececatto <leandrocecatto@hotmail.com>
2017-10-23 21:48:32 -0200

    Esboço de tela de login.

--- app/src/main/java/br/com/uniftec/trabalhofinal/MainActivity.java
+++ app/src/main/java/br/com/uniftec/trabalhofinal/MainActivity.java
@@ -2,12 +2,34 @@ package br.com.uniftec.trabalhofinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btLogin = (Button) findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        TextView tLogin = (TextView) findViewById(R.id.tLogin);
+        TextView tSenha = (TextView) findViewById(R.id.tSenha);
+        if(tLogin.getText().toString().equals("leandro")&&tSenha.getText().toString().equals("123")){
+            alert("Login efetuado com sucesso");
+        }else{
+            alert("Login ou senha incorretos");
+        }
+    }
+
+    private void alert(String s){
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
     }
 }
